6bec5cb1210a13e58655f183b2b2fd1021582818
Trace <46228022+TraceLD@users.noreply.github.com>
2021-01-16 05:40:46 +0000

    Upload files to GitHub

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    purge: [
+        "./public/**/*.html"
+    ],
+    theme: {
+        extend: {
+            colors: {
+                vibrantblue: "#008dff",
+                vibrantpink: "#ff267b"
+            },
+            fontFamily: {
+                body: ["Inter"]
+            }
+        }
+    },
+    variants: {},
+    plugins: []
+}
